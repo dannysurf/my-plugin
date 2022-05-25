7c883bc106dece55c82a76bc4f892872011e31fc
GitHub Action <action@github.com>
2022-05-25 09:25:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/dannysurf/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dannysurf/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dannysurf.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dannysurf.myplugin.MyBundle
 
 class MyApplicationService {
 
